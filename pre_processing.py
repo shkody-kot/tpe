8ddfa47beab5b01032063e0f190a424ac788a3f3
shkody-kot <shkodykot.35@gmail.com>
2023-02-23 20:51:16 -0800

    edited for cleanliness; metadata absent

--- pre_processing.py
+++ pre_processing.py
@@ -1,5 +1,6 @@
 '''
-Primary code by Avani Sathe
+Initial code by Avani Sathe
+Altered by Ira Belyaeva
 Code takes an image's thumbnail and the thumbnail of that image filtered
 and alters the image such that a thumbnail of the image would be identical
 to the filtered thumbnail by changing the minimal amount of pixels'''
@@ -7,6 +8,7 @@ to the filtered thumbnail by changing the minimal amount of pixels'''
 from PIL import Image
 import numpy as np
 
+#get an array of differences between means of two images' thumbnails
 def blockwise_difference(img1, img2, block_y, block_x, block_size):
     cols, rows = img2.size
     #print(str(cols) + " pixels(width) by " + str(rows) + " pixels(height)")
@@ -50,60 +52,6 @@ def blockwise_difference(img1, img2, block_y, block_x, block_size):
     return diff_matrix
             
 
-
-def blockwiseDifference(img1, img2, block_y, block_x, block_size):
-    cols, rows = img1.size
-    print(str(cols) + " pixels(width) by " + str(rows) + " pixels(height)")
-    diff_matrix = []
-    
-    block_cols = cols // block_x
-    block_rows = rows // block_y
-
-    for row in range(0, rows, block_y):
-        for col in range(0, cols, block_x):
-            block1 = np.array(img1.crop((col, row, col + block_cols, row + block_rows)))
-            block_sum_img1 = [(np.sum(block1[:, :, channel]) / block_size) for channel in range(block1.shape[2])]
-            print(block_sum_img1)
-            block2 = np.array(img2.crop((col, row, col + block_cols, row + block_rows)))
-            block_sum_img2 = [(np.sum(block2[:, :, channel]) / block_size) for channel in range(block2.shape[2])]
-            print(block_sum_img2)
-            sum_diff = [
-                np.subtract(int(block_sum_img1[i]), int(block_sum_img2[i]))
-                for i in range(len(block_sum_img1))
-            ]
-            diff_matrix.append(sum_diff)
-    return diff_matrix
-
-
-#distribute the difference of thumbnail blocks among the pixels of that block
-def distribute_pixels(image, pixels_to_distribute):
-    channels = image.split()
-    
-    #for each channel (r, g, b)
-    for channel_num, channel in enumerate(channels):
-        pixels = np.array(channel.getdata())
-        num_to_distribute = pixels_to_distribute[channel_num]
-        total_pixels = len(pixels)
-        average_to_add = int(num_to_distribute / total_pixels)
-        overflow_pixels = num_to_distribute % total_pixels
-        manipulations = []
-        for i, pixel in enumerate(pixels):
-            new_pixel = pixel + average_to_add
-            if i < overflow_pixels:
-                new_pixel += 1
-            if new_pixel > 255:
-                manipulations.append((i, pixel, 255))
-                pixels[i] = 255
-            if new_pixel < 0:
-                manipulations.append((i, pixel, 0))
-                pixels[i] = 0
-            else:
-                manipulations.append((i, pixel, new_pixel))
-                pixels[i] = new_pixel
-        #channels[channel_num] = np.array(pixels).reshape(channel.size)
-        channels[channel_num].putdata(pixels)
-    return Image.merge(image.mode, channels), manipulations
-
 #rewritten
 def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
     num_pixels = width * height
@@ -126,7 +74,6 @@ def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
             add_green = add_green * block_size
             add_blue = add_blue * block_size
             
-            #print("red: " + str(add_red) + " green: " + str(add_green) + " blue: " + str(add_blue))
             #for each pixel in the block
             for pixel in range(block_x * block_y):
                 row = pixel // block_x          #row of pixel (row_num is row of block)
@@ -134,31 +81,22 @@ def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
                 position = row_num * width * block_y + row * width + col_num * block_x + col
                 red, green, blue = img[position]
                 
-                #print("positiom " + str(position) + " -- " + str(img[position]))
-                #print("old values: " + str(red) + " -- " + str(green) + " -- " + str(blue))
-                
                 #add to red channel
                 if red + add_red > 255:
-                    #print("red overflow in pixel " + str(position))
                     add_red -= (255 - red)
                     red = 255
                 elif red + add_red < 0:
-                    #print("red underflow in pixel " + str(position))
-                    #print("old red: " + str(red))
                     add_red += red
                     red = 0
-                    #print("After change red: " + str(red))
                 else:
                     red += add_red
                     add_red = 0
                 
                 #add to green channel
                 if green + add_green > 255:
-                    #print("green overflow in pixel " + str(position))
                     add_green -= (255 - green)
                     green = 255
                 elif green + add_green < 0:
-                    #print("green underflow in pixel " + str(position))
                     add_green += green
                     green = 0
                 else:
@@ -167,11 +105,9 @@ def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
                     
                 #add to blue channel
                 if blue + add_blue > 255:
-                    #print("blue overflow in pixel " + str(position))
                     add_blue -= (255 - blue)
                     blue = 255
                 elif blue + add_blue < 0:
-                    #print("blue underflow in pixel " + str(position))
                     add_blue += blue
                     blue = 0
                 else:
@@ -189,30 +125,6 @@ def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
     print(np.shape(pixels))
     img2 = Image.fromarray(pixels.astype(np.uint8))
     img2.save("modified_image(ira).jpg")          
-
-'''def generate_o_dash(img1, block_cols, block_rows, block_x, block_y, diff_matrix):
-    # Loop over each block
-    for by in range(block_rows):
-        for bx in range(block_cols):
-        # Calculate the starting and ending x and y coordinates of the block
-            start_x = bx * block_x
-            start_y = by * block_y
-            end_x = start_x + block_x
-            end_y = start_y + block_y
-            print("row " + str(by) + " col " + str(bx))
-
-            # Crop the block from the original image
-            block1 = img1.crop((start_x, start_y, end_x, end_y))
-            for i in range(len(diff_matrix)):
-                #print("block " + str(i) + " row " + str(by) + " col " + str(bx))
-                # Loop over each pixel in the block
-                for y in range(block_y):
-                    for x in range(block_x):
-                        block1_after_modification, maniputations = distribute_pixels(block1, diff_matrix[i])
-                        #Paste the modified block back onto the original image
-                        img1.paste(block1_after_modification, (start_x, start_y, end_x, end_y))
-    #Save the modified image
-    img1.save("modified_image.jpg")'''
 
 def create_metadata():
     
