88f99cc6ac4bd23bed751279c6215d831f031f98
shkody-kot <shkodykot.35@gmail.com>
2023-02-28 10:57:10 -0800

    pre processing works

--- pre_processing.py
+++ pre_processing.py
@@ -50,12 +50,15 @@ def blockwise_difference(img1, img2, block_y, block_x, block_size):
             diff_matrix.append(difference)
             
     return diff_matrix
-            
+ 
 
-#rewritten
+#change pixels in the original image using a greedy algorithm to change each pixel
+#as much as possible, therefore reducing metadata to store
 def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
     num_pixels = width * height
     pixels = [0] * num_pixels
+    changes = []            #store info about pixels changed
+    
     print("O_DASH")
     block_rows = height // block_y
     block_cols = width // block_x
@@ -80,6 +83,9 @@ def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
                 col = pixel % block_x           #col of pixel (col_num is col of block)
                 position = row_num * width * block_y + row * width + col_num * block_x + col
                 red, green, blue = img[position]
+                old_r = red
+                old_g = green
+                old_b = blue
                 
                 #add to red channel
                 if red + add_red > 255:
@@ -114,32 +120,159 @@ def generate_o_dash(img, img2, width, height, block_x, block_y, diff_matrix):
                     blue += add_blue
                     add_blue = 0
                 
+                #check if any pixels have been changed
+                if old_r != red or old_g != green or old_b != blue:
+                    #append to changes list
+                    red_tuple = (position, old_r, red)
+                    green_tuple = (position, old_g, green)
+                    blue_tuple = (position, old_b, blue)
+                    changes.append(red_tuple)
+                    changes.append(green_tuple)
+                    changes.append(blue_tuple)
+                
                 #return values to image
                 rgb = (red, green, blue)
                 pixels[position] = rgb
-    #test
+    #test if successful
     test = blockwise_difference(pixels, img2, block_y, block_x, block_x * block_y)
     print(test)
-    #create image in right order
-    pixels = np.reshape(pixels, (height, width, 3))
-    print(np.shape(pixels))
-    img2 = Image.fromarray(pixels.astype(np.uint8))
-    img2.save("modified_image(ira).jpg")          
-
-def create_metadata():
     
-    return
+    #generate metadata
+    m_height, metadata = create_metadata(changes, width)
+    
+    #append metadata to image data
+    metadata = np.reshape(metadata, (m_height, width, 3))    
+    pixels = np.reshape(pixels, (height, width, 3))
+    pixels = np.append(pixels, metadata, 0)
+    
+    #create image in right order
+    #pixels = np.reshape(pixels, (height + m_height, width, 3))
+    print(np.shape(pixels))
+    
+    img_new = Image.fromarray(pixels.astype(np.uint8))
+    img_new.save("modified_image(ira).png")    
+
+#create metadata for changed pixels
+def create_metadata(changes, width):
+    print("METADATA")
+    metadata = [0] * len(changes)
+    
+    #sort the change list so it's by pixel position not by block
+    changes.sort(key = lambda a: a[0])
+    
+    previous_change = 0
+    
+    
+    #change the position value to the distance from last changed pixel, in hopes of
+    #remaining with values less than 255
+    for item in range(0, len(changes), 3):
+        position, old, new = changes[item]
+        temp = position
+        
+        for pixel in range(3):
+            index, old, new = changes[item + pixel]
+            index -= previous_change
+            
+            new_tuple = (index, old, new)
+            metadata[item + pixel] = new_tuple
+        
+        #print("previous change: " + str(previous_change) + " new value: " + str(temp))
+        previous_change = temp
+    
+    #see what i've done
+    #print(metadata)
+    
+    #pad the metadata so it's a multiple of the width
+    height = len(metadata) // width
+    remainder = len(metadata) % width
+    print("rows: " + str(height) + " remainder: " + str(remainder))
+    
+    for pixel in range(width - remainder):
+        metadata.append((0, 0, 0))
+    
+    height += 1    
+    return height, metadata
+
+#undo o_dash pixel changes to restore original image
+def undo_o_dash(img, width, height):
+    #extract the metadata from image
+    print("UNDO O_DASH")
+    img_width, img_height = img.size
+    
+    metadata = [0] * (img_height - height) * width
+    original_image = [0] * width * height
+    
+    m_row = 0
+    
+    #separate the matadata from the image
+    for row in range(height, img_height):
+        #print(row)
+        for col in range(width):
+            index = row * width + col
+            
+            position, old, new = img[index]
+            
+            #not a true change; padding
+            if position == 0 and old == 0 and new == 0:
+                continue
+            
+            new_tuple = (position, old, new)
+            metadata[m_row * width + col] = new_tuple
+        m_row += 1
+    
+    index = len(metadata) - 1
+    while metadata[index] == 0:
+        metadata.pop(index)
+        index -= 1
+    
+    current_index = 0
+    last_changed = 0
+    next = 0
+    
+    #print(metadata)
+    
+    #undo changes applied by o_dash
+    for row in range(height):
+        for col in range(width):
+            position = row * width + col
+            
+            red, green, blue = img[position]
+            
+            #if this is a changed pixel
+            if position == (last_changed + next) and current_index + 3 < len(metadata):
+            
+                #print("changing index " + str(position))
+                nextr, oldr, newr = metadata[current_index]
+                nextb, oldg, newg = metadata[current_index + 1]
+                nextg, oldb, newb = metadata[current_index + 2]
+                
+                if red == newr and blue == newb and green == newg:
+                    #print("change index of metadata: " + str(current_index))
+                    red = oldr
+                    green = oldg
+                    blue = oldb
+                    current_index += 3
+                    last_changed = position
+                    next, old, new = metadata[current_index]
+                else:
+                    next += 1
+                
+            #add pixel back to image
+            rgb = (red, green, blue)
+            original_image[position] = rgb
+    
+    #create image in right order
+    original_image = np.reshape(original_image, (height, width, 3))
+    print(np.shape(original_image))
+    img2 = Image.fromarray(original_image.astype(np.uint8))
+    img2.save("original_image(ira).png")
+            
 
 #MAIN OPERATION
 
 # Open the image (potentially make it accept stream of data as opposed to image url)
 img1 = Image.open("B99_thumbnail.jpg")
 img2 = Image.open("B99_mosaic_med.jpg")
-
-'''
-img1 = original_image_data
-img2 = filtered_image_data (can maybe be thumbnail)
-'''
 
 # Get the width and height of the image
 width, height = img1.size
@@ -152,11 +285,18 @@ block_size = block_x * block_y
 
 print("calculated num pixels: " + str(width * height))
 
-
+#ENCRYPT
 diff_matrix = blockwise_difference(img1.getdata(), img2.getdata(), block_y, block_x, block_size)
 
 print(diff_matrix)
 #print(len(diff_matrix))
 
 generate_o_dash(img1.getdata(), img2.getdata(), width, height - 1, block_x, block_y, diff_matrix)
+
+
+#DECRYPT
+img3 = Image.open("modified_image(ira).png")
+
+undo_o_dash(img3.getdata(), width, height - 1)
+
 
